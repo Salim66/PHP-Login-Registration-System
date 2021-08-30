313fcdbeb13ca74c058930acc024b9e95f75bb2c
Salim66 <salimhasanriad8@gmail.com>
2021-08-30 09:44:24 +0600

    Create general hepler functions

--- functions/functions.php
+++ functions/functions.php
@@ -1,7 +1,52 @@
 <?php
 
 
+    //==================== Create Helper Functions =====================//
 
+    // clean entities, special symbols
+    function clean($string){
+
+        return htmlentities($string);
+
+    }
+
+    // create redirect function
+    function redirect($location){
+
+        return header("Location: {$location}");
+
+    }
+
+    // set session message function
+    function setMessage($message){
+        
+        if(!empty($message)){
+            $_SESSION['message'] = $message;
+        }else {
+            $message = "";
+        }
+
+    }
+
+    // display session message function
+    function displayMessage(){
+
+        if(isset($_SESSION['message'])){
+
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+
+        }
+
+    }
+
+    // Create token genarator function
+    function tokenGenerator(){
+
+        $token = $_SESSION['token'] = md5(uniqid(mt_rand(), true));
+        return $token;
+        
+    }
 
 
 ?>
